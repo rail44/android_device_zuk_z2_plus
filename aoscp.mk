d2f61de772374347554a0c126c5548b451112a70
Satoshi Amemiya <amemiya@protonmail.com>
2018-05-10 22:33:07 +0900

    wip

--- aoscp.mk
+++ aoscp.mk
@@ -20,23 +20,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Boot animation resolution
-# TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common CypherOS stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-# Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
-PRODUCT_NAME := aosp_z2_plus
+PRODUCT_NAME := aoscp_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
 PRODUCT_MODEL := Z2 Plus
 
+PRODUCT_GMS_CLIENTID_BASE := android-zuk
+
+TARGET_VENDOR_PRODUCT_NAME := z2_plus
+TARGET_VENDOR_DEVICE_NAME := z2_plus
+
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=z2_plus PRODUCT_NAME=aosp_z2_plus
 
-# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="ZUK/z2_plus/z2_plus:7.0/NRD90M/3.1.117_171110:user/release-keys" \
     PRIVATE_BUILD_DESC="z2_plus-user 7.0 NRD90M 3.1.117_171110 release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "ZUK/z2_plus/z2_plus:7.0/NRD90M/3.1.117_171110:user/release-keys"
+TARGET_VENDOR := zuk
